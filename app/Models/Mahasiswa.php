791ab06e8256c1148deaab98ec70860996eba3db
antena-kipas <ujankcodet0@gmail.com>
2026-04-28 13:29:30 +0700

    MODELS PATCH 3 mengubah menu.json untuk menampilkan sidebar mahasiswa

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -5,23 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
-class Mahasiswa extends Model 
+class LogKeamanan extends Model 
 {
     use HasUuids;
-    
-    
-    protected $table = 'mahasiswa';
-    
+
+    protected $table = 'log_keamanan';
     protected $primaryKey = 'uuid';
     public $incrementing = false;
-    protected $keyType = 'string';
-    
+    protected $keyType = 'string'; // Tambahan
     
     protected $fillable = [
         'users_id', 
-        'nim'
+        'username_attempt', 
+        'tipe_event', 
+        'ip_address', 
+        'user_agent', 
+        'is_suspicious'
     ];
-    
+
+    protected $casts = [
+        'is_suspicious' => 'boolean',
+    ];
+
     public function user() 
     { 
         return $this->belongsTo(User::class, 'users_id', 'uuid'); 
