9503e542cb3d4c40073c18653a81dc34ceaef3b5
AwilGarut <lesmanapahril@gmail.com>
2026-05-04 13:19:57 +0700

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'SIREKIPEMA')</title>
-
     <link rel="icon" type="image" href="{{ asset('assets/images/landingPages/logo-kabSubang.webp') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
